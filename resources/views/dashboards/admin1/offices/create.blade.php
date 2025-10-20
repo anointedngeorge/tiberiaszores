e31082095a4253e7b574d95a79e77aa0bf8f8035
anointedngeorge <anointedngeorge@gmail.com>
2025-10-20 11:12:32 +0100

    fix: added new items

--- resources/views/dashboards/admin1/offices/create.blade.php
+++ resources/views/dashboards/admin1/offices/create.blade.php
@@ -30,19 +30,19 @@
                                 <div class="row">
                                     <div>
                                         <label>Phone 1</label>
-                                        <input value="{{ $object->phone1 ?? 'sitename'}}" placeholder="Phone number 1"
+                                        <input value="{{ $object->phone1 ?? ''}}" placeholder="Phone number 1"
                                             class="form-control form-control-sm" type="tel" name="phone1">
                                     </div>
                                     <div class="mt-2">
                                         <label>Email Address 1</label>
-                                        <input value="{{ $object->email_address1 ?? 'support@example' }}"
+                                        <input value="{{ $object->email_address1 ?? '' }}"
                                             placeholder="support@example.com" class="form-control form-control-sm"
                                             type="email" name="email_address1">
                                     </div>
 
                                     <div class="mt-2">
                                         <label>Address 1</label>
-                                        <input value="{{ $object->address1 ?? 'support@example' }}"
+                                        <input value="{{ $object->address1 ?? '' }}"
                                             placeholder="Address 1" class="form-control form-control-sm" type="text"
                                             name="address1">
                                     </div>
@@ -52,19 +52,19 @@
                                 <div class="row">
                                     <div>
                                         <label>Phone 2</label>
-                                        <input value="{{ $object->phone2 ?? 'sitename'}}" placeholder="Phone number 2"
+                                        <input value="{{ $object->phone2 ?? ''}}" placeholder="Phone number 2"
                                             class="form-control form-control-sm" type="tel" name="phone2">
                                     </div>
                                     <div class="mt-2">
                                         <label>Email Address 2</label>
-                                        <input value="{{ $object->email_address2 ?? 'support@example' }}"
+                                        <input value="{{ $object->email_address2 ?? '' }}"
                                             placeholder="support@example.com" class="form-control form-control-sm"
                                             type="email" name="email_address2">
                                     </div>
 
                                     <div class="mt-2">
                                         <label>Address 2</label>
-                                        <input value="{{ $object->address2 ?? 'support@example' }}"
+                                        <input value="{{ $object->address2 ?? '' }}"
                                             placeholder="Address 2" class="form-control form-control-sm" type="text"
                                             name="address2">
                                     </div>
